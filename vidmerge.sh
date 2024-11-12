d93e82ff01763ecb375fd83af065a5ab505f4045
Vathanak Mao <vathanakmao@gmail.com>
2024-11-13 06:31:07 +0700

    Fixed 'MError when src file's name contains special char

--- vidmerge.sh
+++ vidmerge.sh
@@ -18,19 +18,30 @@ IFS=":" read -a srcfiles <<< "$filenames"
 
 concat=""
 newnames=()
+tmpfilenames=()
 for srcfile in "${srcfiles[@]}"; do
 	echo "[DEB] srcfile: $srcfile"
 	
+	tmp=$srcfile
+	## if contains spacial characters, make a copy with a new name
+	if [[ "$srcfile" =~ [^a-zA-Z0-9_\(\)\-] ]]; then
+	  tmp=$(date +%N)
+	  cp $srcfile $tmp
+	  tmpfilenames+=("$tmp")
+	  #tmpname=$( echo "$srcfile" | tr " " "_" )
+	  #mv "$srcfile" "$tmpname" && newnames+=("$tmpname")
+	fi
+	
 	## Remove spaces from the names of source videos 
 	## to avoid error when running ffmpeg command
-	tmpname=$( echo "$srcfile" | tr " " "_" )
-	mv "$srcfile" "$tmpname" && newnames+=("$tmpname")
+	#tmpname=$( echo "$srcfile" | tr " " "_" )
+	#mv "$srcfile" "$tmpname" && newnames+=("$tmpname")
 	
 	## Go to https://trac.ffmpeg.org/wiki/Concatenate then section "Using intermediate files"
 	## For "-c:v" option, go to https://ffmpeg.org/ffmpeg.html#Stream-selection
-	ffmpeg -i $tmpname -c:v copy -bsf:v h264_mp4toannexb "$tmpname.ts"
+	ffmpeg -i $tmp -c:v copy -bsf:v h264_mp4toannexb "$tmp.ts"
 	
-	concat+="$tmpname.ts|"
+	concat+="$tmp.ts|"
 	
 	#newnames+=("$tmpname")
 done
@@ -39,13 +50,21 @@ done
 outputname="mergedvideos-$(date +%s).mp4"
 ffmpeg -i "concat:$concat" -c:v copy -bsf:a aac_adtstoasc "$outputname"
 
-## Rename the source videos back to their original ones
-echo "[DEB] newnames: ${newnames[@]}"
-for newname in ${newnames[@]}; do 
-	originalname=$(echo $newname | tr "_" " ")
-	mv "$newname" "$originalname"
+
+
+echo "[DEB] Temp files: ${tmpfilenames[@]}"
+for f in ${tmpfilenames[@]}; do 
+	rm $f
 done
 
+
+## Rename the source videos back to their original ones
+#echo "[DEB] newnames: ${newnames[@]}"
+#for newname in ${newnames[@]}; do 
+#	originalname=$(echo $newname | tr "_" " ")
+#	mv "$newname" "$originalname"
+#done
+
 ## Remove intermeditate files
-rm *.mp4.ts
+rm *.ts
 
